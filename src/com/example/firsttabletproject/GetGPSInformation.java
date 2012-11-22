fcd430a068c8931b998f1c9bb78057084f5d165f
Eric Boeck <eric.boeck@gmail.com>
2012-11-22 12:58:32 -0500

    GPS working

--- src/com/example/firsttabletproject/GetGPSInformation.java
+++ src/com/example/firsttabletproject/GetGPSInformation.java
@@ -38,10 +38,10 @@ public class GetGPSInformation extends Service implements LocationListener {
     double longitude; // longitude
  
     // The minimum distance to change Updates in meters
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 2; // 10 meters
  
     // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 5 * 1; // 5 seconds
  
     // Declaring a Location Manager
     protected LocationManager locationManager;
--- src/com/example/firsttabletproject/MainActivity.java
+++ src/com/example/firsttabletproject/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.firsttabletproject;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.location.Location;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -17,4 +20,32 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    public void onClick_EricsButton(View view){
+    	
+    GetGPSInformation myGPS = new GetGPSInformation(getApplicationContext());
+    
+    if (myGPS.canGetLocation())
+    {
+    	double lat = myGPS.getLatitude();
+    	double lon = myGPS.getLongitude();
+    	
+    	Location myLocation = myGPS.getLocation();
+    	long tme = myLocation.getTime();
+    	
+    	
+    	TextView myLoc = (TextView) findViewById(R.id.tv_Latitude);
+    	myLoc.setText(String.format("%f", lat));
+    	myLoc = (TextView) findViewById(R.id.tv_Longitude);
+    	myLoc.setText(String.format("%f", lon));
+    	myLoc = (TextView) findViewById(R.id.tv_Time);
+    	myLoc.setText(String.format("%tc", tme));
+    	
+    }
+    
+    
+    	
+    }
+    
+    
 }
